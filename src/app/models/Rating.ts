625ee671099c0dec9bd91b735ca5827e76d1b545
chiseciprian <chise.ciprian05@gmail.com>
2021-08-10 21:37:28 +0300

    steli commit

--- src/app/models/Rating.ts
+++ src/app/models/Rating.ts
@@ -1,11 +1,9 @@
-import {Timestamp} from "rxjs";
-
 export interface Rating {
   ratingId: string;
   bookId: string;
   userName: string;
   title: string;
   message: string;
-  date: Timestamp<any>;
+  date: number;
   stars: number;
 }
